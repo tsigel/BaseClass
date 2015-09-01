d85ff6f0d4d5411445f498fcf52c55fa0f5d2460
daniil <d.tsigelnitskiy@netris.ru>
2015-09-01 16:01:06 +0300

    add gulp tasks

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,3 +1,29 @@
-/**
- * Created by daniil on 01.09.15.
- */
+/// <reference path="./typings/tsd.d.ts" />
+
+import gulp = require('gulp');
+import typescript = require('gulp-typescript');
+import uglify = require('gulp-uglify');
+
+gulp.task('common', () => {
+
+    gulp.src('src/Base.ts')
+        .pipe(typescript({
+            module: 'commonjs'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/'));
+
+});
+
+gulp.task('AMD', () => {
+
+    gulp.src('src/Base.ts')
+        .pipe(typescript({
+            module: 'AMD'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/'));
+
+});
+
+gulp.task('default', ['common']);
